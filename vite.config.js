9aa96c0fd651edff9986451040badc307910279b
Being-ananthan <ananthapadmanabhanprakash@gmail.com>
2026-03-20 22:16:53 +0530

    feat: initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    historyApiFallback: true,
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom') || id.includes('node_modules/react-router')) {
-            return 'vendor'
-          }
-        },
-      },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
